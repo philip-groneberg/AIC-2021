859b3339f8222baf64445bc825985401d4f13cd4
theresa.follath <follath@campus.tu-berlin.de>
2021-05-25 21:44:36 +0200

    20 runs + mean

--- KDD_data/cluster_analysis.py
+++ KDD_data/cluster_analysis.py
@@ -11,8 +11,7 @@ from sklearn import preprocessing
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import completeness_score, homogeneity_score, mutual_info_score, fowlkes_mallows_score
-
+from sklearn.metrics import completeness_score, homogeneity_score, mutual_info_score, fowlkes_mallows_score, normalized_mutual_info_score, calinski_harabasz_score, adjusted_mutual_info_score, davies_bouldin_score, silhouette_score
 from matplotlib import cm
 cmap = cm.get_cmap('gist_rainbow')
 
@@ -36,14 +35,14 @@ def plot_2d(data, dim1, dim2, features):
     plt.show()
     return
 
-kdd, labels = getData()
-print('features:', kdd.feature_names)
+#kdd, labels = getData()
+print(len(kdd.feature_names), 'features:', kdd.feature_names)
 
 classes, n_class_sizes = np.unique(kdd.target, return_counts = True)
 n_classes = len(classes)
 
 for t in zip(classes, n_class_sizes):
-    print(f'class: {t[0].astype(str)}, size: {t[1]}')
+    print(f'class: {t[0]}, size: {t[1]}')
 
 # kmeans
 #n_clusters = 5
@@ -55,26 +54,46 @@ for t in zip(classes, n_class_sizes):
 #cluster_id, cluster_sizes = np.unique(kn.labels_, return_counts=True)
 
 # k means for different cluster sizes with metric evaluation
-scores = [[], [], [], []]
-
-for k in range(1,23):
-    kn = KMeans(n_clusters = k)
-    y_pred = kn.fit_predict(kdd.data)
-    #y_pred = kn.predict(kdd.data)
-    cs = completeness_score(labels, y_pred)
-    hom = homogeneity_score(labels, y_pred)
-    mi = mutual_info_score(labels, y_pred)
-    fms = fowlkes_mallows_score(labels, y_pred)
+metrics = ['completeness', 'homogeneity', 'Mutual Info', 'Fowlkes Mallows', 'davies bouldin','calisnki']
+#scores = [[]]*len(metrics)
+scores = [[], [], [], [], [], []]
+rounds = 20
+print(scores)
+for k in range(2, 24):
+    cs = 0
+    hom = 0
+    mi = 0
+    fms = 0
+    chs = 0
+    dbs = 0
+    for j in range(rounds):    
+        kn = KMeans(n_clusters = k)
+        y_pred = kn.fit_predict(kdd.data)
+        cs += completeness_score(labels, y_pred)
+        hom += homogeneity_score(labels, y_pred)
+        mi += adjusted_mutual_info_score(labels, y_pred)
+        fms += fowlkes_mallows_score(labels, y_pred)
+        dbs += davies_bouldin_score(kdd.data.values, labels=y_pred)
+        chs += calinski_harabasz_score(kdd.data.values, y_pred)
     print(f'{k} clusters: cs={cs}, hom={hom}, mi={mi}, fms={fms}')
+    cs = cs/rounds
+    hom= hom/rounds
+    mi = mi/rounds
+    fms = fms/rounds
+    dbs = dbs/rounds
+    chs = chs/rounds
     scores[0] += [cs]
     scores[1] += [hom]
     scores[2] += [mi]
     scores[3] += [fms]
+    scores[4] += [dbs]
+    scores[5] += [chs]
+    cluster_id, cluster_sizes = np.unique(kn.labels_, return_counts=True)
+    print(cluster_sizes)
 
-metrics = ['completeness', 'homogeneity', 'Mutual Info', 'Fowlkes Mallows']
 plt.figure(figsize=(9,6))
-for i in range(4):
-    plt.plot(np.arange(1,len(scores[0])), scores[i], label = metrics[i])
+for i in range(len(metrics)-1):
+    plt.plot(np.arange(0,len(scores[0])), scores[i], label = metrics[i])
 plt.xlabel('Cluster size', fontsize=15)
 plt.ylabel('Metric result', fontsize=15)
 plt.legend(fontsize=15)
