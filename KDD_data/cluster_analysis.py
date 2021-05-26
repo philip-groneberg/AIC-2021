8cfa20247debe64d38fb676adda095f40d84f026
theresa.follath <follath@campus.tu-berlin.de>
2021-05-27 00:06:20 +0200

    20 rounds

--- KDD_data/cluster_analysis.py
+++ KDD_data/cluster_analysis.py
@@ -8,12 +8,9 @@ Created on Fri May 14 09:47:15 2021
 from sklearn.datasets import fetch_kddcup99
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
-import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import completeness_score, homogeneity_score, mutual_info_score, fowlkes_mallows_score, normalized_mutual_info_score, calinski_harabasz_score, adjusted_mutual_info_score, davies_bouldin_score, silhouette_score
-from matplotlib import cm
-cmap = cm.get_cmap('gist_rainbow')
 
 def getData():
     kdd = fetch_kddcup99(as_frame = True)
@@ -35,7 +32,7 @@ def plot_2d(data, dim1, dim2, features):
     plt.show()
     return
 
-#kdd, labels = getData()
+kdd, labels = getData()
 print(len(kdd.feature_names), 'features:', kdd.feature_names)
 
 classes, n_class_sizes = np.unique(kdd.target, return_counts = True)
@@ -44,19 +41,11 @@ n_classes = len(classes)
 for t in zip(classes, n_class_sizes):
     print(f'class: {t[0]}, size: {t[1]}')
 
-# kmeans
-#n_clusters = 5
-#kn = KMeans(n_clusters = n_clusters)
-#kn.fit(kdd.data)
-
-#cluster = kn.predict(kdd.data)
-#centers = kn.cluster_centers_
-#cluster_id, cluster_sizes = np.unique(kn.labels_, return_counts=True)
 
 # k means for different cluster sizes with metric evaluation
-metrics = ['completeness', 'homogeneity', 'Mutual Info', 'Fowlkes Mallows', 'davies bouldin','calisnki']
-#scores = [[]]*len(metrics)
-scores = [[], [], [], [], [], []]
+metrics = ['Completeness', 'Homogeneity', 'Adjusted Mutual Information', 'Fowlkes Mallows']
+
+scores = [[], [], [], []]
 rounds = 20
 print(scores)
 for k in range(2, 24):
@@ -64,8 +53,6 @@ for k in range(2, 24):
     hom = 0
     mi = 0
     fms = 0
-    chs = 0
-    dbs = 0
     for j in range(rounds):    
         kn = KMeans(n_clusters = k)
         y_pred = kn.fit_predict(kdd.data)
@@ -73,56 +60,23 @@ for k in range(2, 24):
         hom += homogeneity_score(labels, y_pred)
         mi += adjusted_mutual_info_score(labels, y_pred)
         fms += fowlkes_mallows_score(labels, y_pred)
-        dbs += davies_bouldin_score(kdd.data.values, labels=y_pred)
-        chs += calinski_harabasz_score(kdd.data.values, y_pred)
-    print(f'{k} clusters: cs={cs}, hom={hom}, mi={mi}, fms={fms}')
+        
     cs = cs/rounds
     hom= hom/rounds
     mi = mi/rounds
     fms = fms/rounds
-    dbs = dbs/rounds
-    chs = chs/rounds
+    print(f'{k} clusters: cs={cs}, hom={hom}, mi={mi}, fms={fms}')
     scores[0] += [cs]
     scores[1] += [hom]
     scores[2] += [mi]
     scores[3] += [fms]
-    scores[4] += [dbs]
-    scores[5] += [chs]
     cluster_id, cluster_sizes = np.unique(kn.labels_, return_counts=True)
     print(cluster_sizes)
 
 plt.figure(figsize=(9,6))
-for i in range(len(metrics)-1):
-    plt.plot(np.arange(0,len(scores[0])), scores[i], label = metrics[i])
+for i in range(len(metrics)):
+    plt.plot(np.arange(2,24), scores[i], label = metrics[i])
 plt.xlabel('Cluster size', fontsize=15)
 plt.ylabel('Metric result', fontsize=15)
 plt.legend(fontsize=15)
 plt.show()
-
-'''
-features = kdd.feature_names
-dim1 = 4    # 4 good
-dim2 = 8 # 8,7,6,0 bad
-f, ax = plt.subplots(nrows=1, ncols = 2, sharey=True, figsize = (12,9))
-
-rgba_values = cmap(np.arange(n_clusters) / n_clusters)
-c = rgba_values[kn.labels_] # color for each point, points with same label have the same color
-ax[0].scatter(kdd.data[features[dim1]], kdd.data[features[dim2]], color=c, s=5)
-ax[0].set_xlabel(features[dim1])
-ax[0].set_title("clusters")
-ax[0].set_ylabel(features[dim2])
-
-# true labels
-rgba_values = cmap(np.arange(n_classes) / n_classes)
-c = rgba_values[labels] # color for each point, points with same label have the same color
-ax[1].scatter(kdd.data[features[dim1]], kdd.data[features[dim2]], color=c, s=5)
-ax[1].set_xlabel(features[dim1])
-ax[1].set_ylabel(features[dim2])
-ax[1].set_title("true classes")
-
-plt.show()
-#dim1 = 0
-#dim2 = 1
-#plot_2d(centers, dim1, dim2, features)
-#plot_2d(centers, 1, 2, features)
-'''
